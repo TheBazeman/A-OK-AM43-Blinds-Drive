c021c6932e951f5fe9d6e8a4de4006511c87b171
TheBazeman <45493950+TheBazeman@users.noreply.github.com>
2020-02-13 10:56:13 +0100

    Version 1.1; Added battery status, current position and amount of light through new CheckStatus command
    Disabled the scanning function, seems to work reliable without it. Return values are in JSON format so you can interpret it easily

--- AOK-AM43.py
+++ AOK-AM43.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # To install libraries needed:
 # sudo pip3 install Flask, bluepy, retrying
-# Version 1.0 - Bas Bahlmann - The Netherlands
+# Version 1.1 - Bas Bahlmann - The Netherlands
+# Added battery status, current position and amount of light CheckStatus command. Disabled the scanning function, seems to work reliable without it
+# Return values are in JSON format so you can interpret it easily
 
-#curl -i http://localhost:5000/AM43BlindsAction/Close
-#curl -i http://localhost:5000/AM43BlindsAction/Open
+#curl -i http://localhost:5000/AM43BlindsAction/Close  --> Closing blinds
+#curl -i http://localhost:5000/AM43BlindsAction/Open  --> Opening blinds
+#curl -i http://localhost:5000/AM43BlindsAction/CheckStatus --> Get battery status, current position and light in %
 
 from bluepy import btle
 import configparser
@@ -12,12 +15,55 @@ import os
 from flask import Flask
 import datetime
 from retrying import retry
+import json
+
 
 #Variables
 config = configparser.ConfigParser() #Read ini file for meters
-config.read('//A-OK_AM43_Blind_Drive//AOK-AM43.ini')
+inifilepath = "/A-OK_AM43_Blind_Drive/AOK-AM43.ini"
 app = Flask(__name__)
 
+# AM43 Notification Identifiers
+# Msg format: 9a <id> <len> <data * len> <xor csum>
+IdMove = 0x0d
+IdStop = 0x0a
+IdBattery = 0xa2
+IdLight = 0xaa
+IdPosition = 0xa7
+IdPosition2 = 0xa8
+IdPosition3 = 0xa9
+
+BatteryPct = None
+LightPct = None
+PositionPct = None
+
+#Check and read inifile
+if (os.path.exists(inifilepath)):
+    config.read(inifilepath)
+else:
+    print()
+    print("ERROR: Cannot find ini file: " + inifilepath + "! Correct the path in this script or put the ini file in the correct directory. Exiting", flush=True)
+    print()
+    exit(1)
+
+class AM43Delegate(btle.DefaultDelegate):
+    def __init__(self):
+        btle.DefaultDelegate.__init__(self)
+    def handleNotification(self, cHandle, data):
+        if (data[1] == IdBattery):
+            global BatteryPct
+            #print("Battery: " + str(data[7]) + "%")
+            BatteryPct = data[7]
+        elif (data[1] == IdPosition):
+            global PositionPct
+            #print("Position: " + str(data[5]) + "%")
+            PositionPct = data[5]
+        elif (data[1] == IdLight):
+            global LightPct
+            #print("Light: " + str(data[3]) + "%")
+            LightPct = data[3]
+        else:
+            print("Unknown identifier notification recieved: " + str(data[1:2]))
 
 @retry(stop_max_attempt_number=2,wait_fixed=2000)
 def ScanForBTLEDevices():
@@ -51,9 +97,9 @@ def ScanForBTLEDevices():
 
 
 @retry(stop_max_attempt_number=3,wait_fixed=2000)
-def ConnectBTLEDevice(AM43BlindsDeviceMacAddress):        
+def ConnectBTLEDevice(AM43BlindsDeviceMacAddress,AM43BlindsDevice):        
     try:
-        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " Connecting to " + AM43BlindsDeviceMacAddress + "...", flush=True)
+        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " Connecting to " + AM43BlindsDeviceMacAddress + ", " + AM43BlindsDevice.capitalize() + "...", flush=True)
         dev = btle.Peripheral(AM43BlindsDeviceMacAddress)
         return dev
     except:
@@ -70,11 +116,15 @@ def AM43BlindsAction(BlindsAction):
     #Variables#
     OpenBlinds = b"\x00\xff\x00\x00\x9a\x0d\x01\x00\x96"
     CloseBlinds = b"\x00\xff\x00\x00\x9a\x0d\x01\x64\xf2"
+    StatusRequests = b"\x00\xff\x00\x00\x9a\xa2\x01\x01\x38", b"\x00\xff\x00\x00\x9a\xaa\x01\x01\x30", b"\x00\xff\x00\x00\x9a\xa7\x01\x01\x3d" ##Battery,Light,Postion
 
+    ResultDict = {}
+    
     #Code#
     # Scan for BTLE devices
     try:
-        ScanForBTLEDevices()
+        #ScanForBTLEDevices()
+        pass
     except:
         print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ERROR SCANNING FOR ALL BTLE Devices, trying to " + BlindsAction + " the blinds anyway....", flush=True)
         print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " Please check any open connections to the blinds motor and close them, the Blinds Engine App perhaps?", flush=True)
@@ -85,38 +135,77 @@ def AM43BlindsAction(BlindsAction):
         bSuccess = False
         AM43BlindsDeviceMacAddress = config.get('AM43_BLE_Devices', AM43BlindsDevice)  # Read BLE MAC from ini file
         try:
-            dev = ConnectBTLEDevice(AM43BlindsDeviceMacAddress)
+            dev = ConnectBTLEDevice(AM43BlindsDeviceMacAddress,AM43BlindsDevice)
         except:
             print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ERROR, Cannot connect to " + AM43BlindsDeviceMacAddress, flush=True)
+            print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " Please check any open connections to the blinds motor and close them, the Blinds Engine App perhaps?", flush=True)
             continue
         
-        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " --> Connected to " + dev.addr, flush=True)
+        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " --> Connected to " + dev.addr + ", " + AM43BlindsDevice.capitalize(), flush=True)
 
-        BlindsControlService = dev.getServiceByUUID("fe50")
-        if (BlindsControlService):
-            BlindsControlServiceCharacteristic = BlindsControlService.getCharacteristics("fe51")[0]
-            if (BlindsAction == "Open"):
-                result = BlindsControlServiceCharacteristic.write(OpenBlinds)
-            elif (BlindsAction == "Close"):
-                result = BlindsControlServiceCharacteristic.write(CloseBlinds)
-            else:
-                print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " Unknown Blindsaction command: " + BlindsAction, flush=True)
-                bSuccess = False
-                result = None
+        if (BlindsAction == "Open" or BlindsAction == "Close"):
+            BlindsControlService = dev.getServiceByUUID("fe50")
+            if (BlindsControlService):
+                BlindsControlServiceCharacteristic = BlindsControlService.getCharacteristics("fe51")[0]
+                if (BlindsAction == "Open"):
+                    result = BlindsControlServiceCharacteristic.write(OpenBlinds)
+                elif (BlindsAction == "Close"):
+                    result = BlindsControlServiceCharacteristic.write(CloseBlinds)
             
             if (result["rsp"][0] == "wr"):
-                print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " --> Writing to " + AM43BlindsDevice + " was succesfull!", flush=True)
+                print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ----> Writing " + BlindsAction + " to " + AM43BlindsDevice.capitalize()  + " was succesfull!", flush=True)
                 bSuccess = True
             else:
-                print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " --> Writing to " + AM43BlindsDevice + " FAILED", flush=True)
+                print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ----> Writing to " + AM43BlindsDevice.capitalize()  + " FAILED", flush=True)
                 bSuccess = False
+            ResultDict.update({AM43BlindsDevice.capitalize(): [{"command":BlindsAction, "bSuccess":bSuccess}]})
+            
+        elif (BlindsAction == "CheckStatus"):
+            BlindsControlService = dev.getServiceByUUID("fe50")
+            if (BlindsControlService):
+                BlindsControlServiceCharacteristic = BlindsControlService.getCharacteristics("fe51")[0]
+                if BlindsControlServiceCharacteristic.supportsRead():
+                    result = dev.setDelegate(AM43Delegate())
+                    for request in StatusRequests:
+                        #print(request)
+                        result = BlindsControlServiceCharacteristic.write(request)
+                        if (result["rsp"][0] == "wr"):
+                            if (dev.waitForNotifications(10)):
+                                #print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " -->  BTLE Notification recieved", flush=True)
+                                pass
+
+                    global BatteryPct #retrieve global variable with current percentages
+                    global LightPct #retrieve global variable with current percentages
+                    global PositionPct #retrieve global variable with current percentages
+                    if (BatteryPct is not None):
+                        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ----> Battery level: " + str(BatteryPct) + "%", flush=True)
+                    if (LightPct is not None):
+                        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ----> Light sensor level: " + str(LightPct) + "%", flush=True)
+                    if (PositionPct is not None):
+                        print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ----> Blinds position: " + str(PositionPct) + "%", flush=True)
+                        
+                    bSuccess = True
+                    ResultDict.update({AM43BlindsDevice.capitalize(): [{"battery":BatteryPct, "position":PositionPct, "light":LightPct}]})
+                else:
+                    print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " ----> No reads allowed on characteristic!", flush=True)
+                
+                BatteryPct = None
+                LightPct = None
+                PositionPct = None
+        else:
+            print(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + " --> Unknown Blindsaction command: " + BlindsAction, flush=True)
+            bSuccess = False
+            result = None
+                    
         # Close connection to BLE device
         dev.disconnect()
-        
+
     if (bSuccess):
-        return "OK\n"
+        ResultDict.update({"status":"OK"})
     else:
-        return "ERROR\n"
+        ResultDict.update({"status":"ERROR"})
+    #return json.dumps(ResultDict) + "\n"  #Oneliner result if you would like
+    return json.dumps(ResultDict, indent=4, sort_keys=True) + "\n"
 
 
 if __name__ == "__main__":
